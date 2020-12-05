6bc8c7cd205f7bcea25f02b01989df3f9d7c653b
maflAT <13979550+maflAT@users.noreply.github.com>
2020-12-05 11:06:34 +0100

    Added feature to reset animation to specific frame. Added README

--- Animation.lua
+++ Animation.lua
@@ -1,27 +1,32 @@
 -- Animation base class:
 -- takes a list of frames and returns the current frame on 'draw'
 -- 'frames' can be a 1-indexed array of any type
--- 'updateRate' is the time in seconds between frames
+-- 'frameTime' is the time in seconds between frames
 
 return Class {
-    init = function(self, frames, updateRate)
+    init = function(self, frames, frameTime)
         self.frames = frames
-        self.updateRate = updateRate
+        self.frameTime = frameTime
         self:reset()
     end,
-    reset = function(self)
-        self.timeout = self.updateRate
-        self.currentFrame = 1
+
+    -- reset animation to startFrame or first frame if no startFrame is supplied
+    reset = function(self, startFrame)
+        self.timer = self.frameTime
+        self.currentFrame = startFrame and startFrame >= 1 or 1
     end,
+
+    -- update current frame based on delta time; 
+    -- return true if animation loop has been completed
     update = function(self, dt)
-        self.timeout = self.timeout - dt
-        if self.timeout <= 0 then
-            self.timeout = self.timeout + self.updateRate
+        self.timer = self.timer - dt
+        if self.timer <= 0 then
+            self.timer = self.timer + self.frameTime
             self.currentFrame = self.currentFrame % #self.frames + 1
-            -- return true if loop has been completed
             return self.currentFrame == 1 and true or false
         end
     end,
+
     draw = function(self)
         return self.frames[self.currentFrame]
     end
---
+++ README.md
@@ -0,0 +1,34 @@
+Revenge of the Prairie King
+===========================
+
+CS50x - Final Project
+---------------------
+
+For my final project, I plan to create a 2D-shooter game in Lua 
+using the LÖVE 2D framework.  
+It will be a *twin-stick shooter* kind of game, featuring a top-down perspective.
+It is inspired by titles such as *Enter the Gungeon* or more realisticly 
+*Journey of the Prairie King*.
+
+I plan to use this awesome Old West style asset pack by [Vaca Roxa](https://twitter.com/bakudas)
+that I found on [itch.io](https://bakudas.itch.io/generic-oldwest-pack).  
+It features sprites for a player and different mobs with walking and attack 
+animations from multiple perspectives.
+
+As a minimum, the game should include the following features:
+
+- Input processing for movement and shooting
+- Animated player
+- Animated melee enemies
+- Collision detection between player and enemies
+- Hit detection between bullets and enemies
+- Flat, open level with randomly generated highlights
+- Win and lose conditions
+
+Depending on how it goes, I might also include some additional features:
+
+- Ranged Enemies
+- Ostacles
+- Power-Ups
+- Multiple maps
+- Scrollable maps
